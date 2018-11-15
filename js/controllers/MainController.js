fcd111bde707f656a80e4154ee9539bdabeaeec8
sharique2523 <sharique2523@gmail.com>
2018-11-15 18:48:05 +0530

    Set limit to currencies

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,27 @@
-app.controller('MainController', ['$scope', 'CryptoProceTrackerApp', function($scope, CryptoProceTrackerApp) {
+app.controller('MainController', ['$scope', '$window', 'CryptoProceTrackerApp', function($scope, $window, CryptoProceTrackerApp) {
+
+	$scope.minMaxValues = {
+		'Bitcoin': {
+			'minValue': 0,
+			'maxValue': 10000
+		}, 
+		'Litecoin': {
+			'minValue': 0,
+			'maxValue': 10000
+		}, 
+		'Ethereum': {
+			'minValue': 0,
+			'maxValue': 10000
+		}, 
+		'XRP': {
+			'minValue': 0,
+			'maxValue': 10000
+		}, 
+		'Stellar': {
+			'minValue': 0,
+			'maxValue': 10000
+		}
+	};
 
   $scope.$on('got new data!', function(event, args) {
     var requiredCryptoCurrencies = ['Bitcoin', 'Litecoin', 'Ethereum', 'XRP', 'Stellar']
@@ -7,7 +30,11 @@ app.controller('MainController', ['$scope', 'CryptoProceTrackerApp', function($s
   	for(var key in data.data) {
   		if(requiredCryptoCurrencies.indexOf(data.data[key].name) != -1) {
   			latestPrice[data.data[key].name] = data.data[key];
-  		}
+				if(data.data[key].quotes.USD.price > $scope.minMaxValues[data.data[key].name].maxValue
+					|| data.data[key].quotes.USD.price < $scope.minMaxValues[data.data[key].name].minValue) {
+					$window.alert("Value of " + data.data[key].name + " i.e. " + data.data[key].quotes.USD.price +" is not in range");
+				}
+			}
   	}
     $scope.cryptoCurrenciesPrice = latestPrice;
   });
